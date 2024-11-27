8259e92b2a89ef9cff01a8eca03518b097145629
Jujubileuu <108826449+Jujubileuu@users.noreply.github.com>
2024-11-27 09:26:55 -0300

    Fixed Menu

--- gametest/MenuScreen/menu.gd
+++ gametest/MenuScreen/menu.gd
@@ -2,8 +2,12 @@ extends Control
 
 var level = "res://main.tscn"
 
-func _on_btn_play_click_end():
+func _ready():
+	pass
+
+func _on_btn_play_pressed():
 	var _level = get_tree().change_scene_to_file(level)
 
-func _on_btn_exit_click_end():
+
+func _on_btn_exit_pressed():
 	get_tree().quit()
